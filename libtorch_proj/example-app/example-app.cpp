b15e4db752a33f439bf5202bc5330e93bbfd10a4
syabyr <mybays@sina.com>
2022-07-25 21:54:46 +0800

    cuda

--- libtorch_proj/example-app/example-app.cpp
+++ libtorch_proj/example-app/example-app.cpp
@@ -1,7 +1,15 @@
 #include <torch/torch.h>
 #include <iostream>
 
+using namespace torch;  // libtorch命名空间
+using namespace std;
+
 int main() {
+  cout << torch::cuda::is_available() << endl;
+  cout << torch::cuda::cudnn_is_available() << endl;
+  cout << torch::cuda::device_count() << endl;
+  std::cout << "CUDA DEVICE COUNT: " << torch::cuda::device_count() << std::endl;
+  
   torch::Tensor tensor = torch::rand({2, 3});
   std::cout << tensor << std::endl;
 }
